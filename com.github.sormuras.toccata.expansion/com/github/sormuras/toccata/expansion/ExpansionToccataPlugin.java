792952a0fb3f8b052cb9797faf3c25f82076d93a
Christian Stein <sormuras@gmail.com>
2021-02-19 22:50:23 +0100

    Load texture from this expansion's assets
    Thanks @almasb!

--- com.github.sormuras.toccata.expansion/com/github/sormuras/toccata/expansion/ExpansionToccataPlugin.java
+++ com.github.sormuras.toccata.expansion/com/github/sormuras/toccata/expansion/ExpansionToccataPlugin.java
@@ -1,20 +1,19 @@
 package com.github.sormuras.toccata.expansion;
 
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.texture.Texture;
 import com.github.sormuras.toccata.Toccata;
 import com.github.sormuras.toccata.ToccataPlugin;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 public class ExpansionToccataPlugin implements ToccataPlugin {
   @Override
   public void initGame(Toccata toccata) {
+    var ball = "/com/github/sormuras/toccata/expansion/assets/textures/ball.png";
     FXGL.entityBuilder()
         .at(100, 100)
         .view(new Text("ExpansionEntity"))
-        .view(new Circle(25, 25, 25, Color.MAGENTA))
-        .view("ball.png")
+        .view(new Texture(FXGL.getAssetLoader().loadImage(getClass().getResource(ball))))
         .buildAndAttach();
   }
 }
