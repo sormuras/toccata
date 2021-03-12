f72cb13a9589557599a5b9c9e28bc1166edf7f71
Christian Stein <sormuras@gmail.com>
2021-03-12 19:12:20 +0100

    Format

--- com.github.sormuras.toccata/com/github/sormuras/toccata/ToccataEntityFactory.java
+++ com.github.sormuras.toccata/com/github/sormuras/toccata/ToccataEntityFactory.java
@@ -10,26 +10,26 @@ import javafx.geometry.Point2D;
 
 public class ToccataEntityFactory implements EntityFactory {
 
-    private static final String prefix = "/com/github/sormuras/toccata/assets/textures/";
+  private static final String prefix = "/com/github/sormuras/toccata/assets/textures/";
 
-    @Spawns("enemy")
-    public Entity newEnemy(SpawnData data) {
+  @Spawns("enemy")
+  public Entity newEnemy(SpawnData data) {
 
-        return FXGL.entityBuilder(data)
-                .view(prefix + "rocket.png")
-                .with(
-                        new ProjectileComponent(
-                                new Point2D(FXGL.random(-1d, 1d), FXGL.random(-1d, 1d)), FXGL.random(50, 200)))
-                .build();
-    }
+    return FXGL.entityBuilder(data)
+        .view(prefix + "rocket.png")
+        .with(
+            new ProjectileComponent(
+                new Point2D(FXGL.random(-1d, 1d), FXGL.random(-1d, 1d)), FXGL.random(50, 200)))
+        .build();
+  }
 
-    @Spawns("ally")
-    public Entity newAlly(SpawnData data) {
-        return FXGL.entityBuilder(data)
-                .view(prefix + "points.png")
-                .with(
-                        new ProjectileComponent(
-                                new Point2D(FXGL.random(-1d, 1d), FXGL.random(-1d, 1d)), FXGL.random(50, 200)))
-                .build();
-    }
+  @Spawns("ally")
+  public Entity newAlly(SpawnData data) {
+    return FXGL.entityBuilder(data)
+        .view(prefix + "points.png")
+        .with(
+            new ProjectileComponent(
+                new Point2D(FXGL.random(-1d, 1d), FXGL.random(-1d, 1d)), FXGL.random(50, 200)))
+        .build();
+  }
 }
