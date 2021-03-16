dd3fb11e0689c1834836c049655f00bb9612f9b3
Christian Stein <sormuras@gmail.com>
2021-03-16 04:43:11 +0100

    Update Bach 17-ea

--- .bach/bach.info/bach/info/Builder.java
+++ .bach/bach.info/bach/info/Builder.java
@@ -1,17 +1,10 @@
 package bach.info;
 
 import com.github.sormuras.bach.Bach;
-import com.github.sormuras.bach.Command;
 import com.github.sormuras.bach.Options;
 import com.github.sormuras.bach.ProjectInfo;
-import com.github.sormuras.bach.lookup.JavaFXModuleLookup;
-import com.github.sormuras.bach.lookup.ModuleLookup;
 import com.github.sormuras.bach.project.Libraries;
 import com.github.sormuras.bach.project.Settings;
-import java.nio.file.DirectoryNotEmptyException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 
 public class Builder extends Bach {
 
@@ -29,78 +22,6 @@ public class Builder extends Bach {
 
   @Override
   public Libraries computeProjectLibraries(ProjectInfo info, Settings settings) {
-    var jacksonVersion = "2.12.1";
-    var attachVersion = "4.0.10";
-    return super.computeProjectLibraries(info, settings)
-        .with(new JavaFXModuleLookup("16"))
-        .with(new FXGLModuleLookup(this, "11.14"))
-        .with(
-            ModuleLookup.external("com.fasterxml.jackson.annotation")
-                .via("com.fasterxml.jackson.core:jackson-annotations:" + jacksonVersion))
-        .with(
-            ModuleLookup.external("com.fasterxml.jackson.core")
-                .via("com.fasterxml.jackson.core:jackson-core:" + jacksonVersion))
-        .with(
-            ModuleLookup.external("com.fasterxml.jackson.databind")
-                .via("com.fasterxml.jackson.core:jackson-databind:" + jacksonVersion))
-        .with(
-            ModuleLookup.external("com.gluonhq.attach.audio")
-                .via("com.gluonhq.attach:audio:" + attachVersion))
-        .with(
-            ModuleLookup.external("com.gluonhq.attach.lifecycle")
-                .via("com.gluonhq.attach:lifecycle:" + attachVersion))
-        .with(
-            ModuleLookup.external("com.gluonhq.attach.storage")
-                .via("com.gluonhq.attach:storage:" + attachVersion))
-        .with(
-            ModuleLookup.external("com.gluonhq.attach.util")
-                .via("com.gluonhq.attach:util:" + attachVersion));
-  }
-
-  @Override
-  public void buildProjectMainSpace() throws Exception {
-
-    super.buildProjectMainSpace();
-
-    var toccata = "com.github.sormuras.toccata";
-    var expansion = "com.github.sormuras.toccata.expansion";
-    var paths = List.of(folders().workspace("modules"), folders().externalModules());
-    var image = deleteDirectories(folders().workspace("image"));
-    run(
-        Command.jlink()
-            .add("--launcher", "toccata=" + toccata)
-            .add("--add-modules", toccata)
-            .add("--add-modules", expansion)
-            .add("--module-path", paths)
-            .add("--output", image));
-
-    if (Boolean.getBoolean("jpackage"))
-      run(
-          Command.of("jpackage")
-              .add("--verbose")
-              .add("--name", "Toccata")
-              .add("--description", "Tower Crush Cannon Tavern")
-              .add("--vendor", "Christian Stein")
-              .add("--runtime-image", image)
-              .add("--module", toccata)
-              .add("--dest", folders().workspace("package")));
-  }
-
-  static Path deleteDirectories(Path directory) {
-    try {
-      Files.deleteIfExists(directory);
-      return directory;
-    } catch (DirectoryNotEmptyException ignored) {
-      // fall through
-    } catch (Exception e) {
-      throw new RuntimeException("Delete directories failed: " + directory, e);
-    }
-    try (var stream = Files.walk(directory)) {
-      var selected = stream.sorted((p, q) -> -p.compareTo(q));
-      for (var path : selected.toArray(Path[]::new)) Files.deleteIfExists(path);
-    } catch (Exception e) {
-      throw new RuntimeException("Delete directories failed: " + directory, e);
-    }
-    return directory;
+    return super.computeProjectLibraries(info, settings).with(new FXGLModuleLookup(this, "11.14"));
   }
 }
--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -1,5 +1,8 @@
 import com.github.sormuras.bach.ProjectInfo;
 import com.github.sormuras.bach.ProjectInfo.External;
+import com.github.sormuras.bach.ProjectInfo.Externals;
+import com.github.sormuras.bach.ProjectInfo.Externals.Name;
+import com.github.sormuras.bach.ProjectInfo.Tools;
 import com.github.sormuras.bach.ProjectInfo.Tweak;
 import com.github.sormuras.bach.project.JavaStyle;
 
@@ -7,19 +10,38 @@ import com.github.sormuras.bach.project.JavaStyle;
     name = "toccata",
     version = "1-ea",
     format = JavaStyle.GOOGLE,
-    requires = {"com.almasb.fxgl.all", "jdk.crypto.ec"},
-    lookupExternal =
-        @External(module = "kotlin.stdlib", via = "org.jetbrains.kotlin:kotlin-stdlib:1.4.30"),
-    tweaks = {
-      @Tweak(
-          tool = "jar(com.github.sormuras.toccata)",
-          option = "-C",
-          value = {"com.github.sormuras.toccata", "."}),
-      @Tweak(
-          tool = "jar(com.github.sormuras.toccata.expansion)",
-          option = "-C",
-          value = {"com.github.sormuras.toccata.expansion", "."}),
-    })
+    includeSourceFilesIntoModules = true,
+    tools = @Tools(skip = "javadoc"),
+    tweaks =
+        @Tweak(
+            tool = "jlink",
+            option = "--launcher",
+            value = "toccata=com.github.sormuras.toccata/com.github.sormuras.toccata.Toccata"),
+    // <editor-fold desc="Libraries - External Modules">
+    lookupExternal = {
+      // Jackson
+      @External(
+          module = "com.fasterxml.jackson.annotation",
+          via = "com.fasterxml.jackson.core:jackson-annotations:2.12.1"),
+      @External(
+          module = "com.fasterxml.jackson.core",
+          via = "com.fasterxml.jackson.core:jackson-core:2.12.1"),
+      @External(
+          module = "com.fasterxml.jackson.databind",
+          via = "com.fasterxml.jackson.core:jackson-databind:2.12.1"),
+      // Gluon Attach
+      @External(module = "com.gluonhq.attach.audio", via = "com.gluonhq.attach:audio:4.0.10"),
+      @External(
+          module = "com.gluonhq.attach.lifecycle",
+          via = "com.gluonhq.attach:lifecycle:4.0.10"),
+      @External(module = "com.gluonhq.attach.storage", via = "com.gluonhq.attach:storage:4.0.10"),
+      @External(module = "com.gluonhq.attach.util", via = "com.gluonhq.attach:util:4.0.10"),
+      // Kotlin
+      @External(module = "kotlin.stdlib", via = "org.jetbrains.kotlin:kotlin-stdlib:1.4.30"),
+    },
+    lookupExternals = @Externals(name = Name.JAVAFX, version = "16")
+    // </editor-fold>
+    )
 module bach.info {
   requires com.github.sormuras.bach;
 
